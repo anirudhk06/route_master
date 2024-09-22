2a2b86b06a234239f5792cb5774b8f634866df9b
anirudhk06 <anirudhani300@gmail.com>
2024-09-22 12:49:15 +0530

    restructure the User model

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -37,18 +37,6 @@ class UserManager(BaseUserManager):
         self.create_user(email, username, password, **other_fields)
 
 
-class UserRoles(models.Model):
-    organizer = "organizer"
-    guide = "guide"
-    attendee = "attendee"
-
-    ROLE_CHOICE = (
-        (organizer, _("Organizer")),
-        (guide, _("Guide")),
-        (attendee, _("Attendee")),
-    )
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     class Role(models.TextChoices):
         ORGANIZER = "organizer", _("Organizer")
@@ -58,69 +46,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
     username = models.CharField(max_length=60, unique=True)
     name = models.CharField(max_length=100, null=True, blank=True)
-    role = models.ManyToManyField(UserRoles)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
-    base_role = Role.ORGANIZER
+    role = models.CharField(max_length=40, choices=Role.choices, default=Role.ATTENDEE)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
-    def save(self, *args, **kwargs) -> None:
-        if not self.pk:
-            self.role = self.base_role
-
-        return super().save(*args, **kwargs)
-
     def __str__(self) -> str:
         return str(self.username)
-
-
-class OrganizerManager(BaseUserManager):
-    def get_queryset(self, *args, **kwargs) -> models.QuerySet:
-        qs = super().get_queryset(*args, **kwargs)
-        return qs.filter(role=User.Role.ORGANIZER)
-
-
-class Organizer(User):
-    base_role = User.Role.ORGANIZER
-
-    objects = OrganizerManager()
-
-    class Meta:
-        proxy = True
-
-
-class GuideManager(BaseUserManager):
-    def get_queryset(self, *args, **kwargs) -> models.QuerySet:
-        qs = super().get_queryset(*args, **kwargs)
-        return qs.filter(role=User.Role.GUIDE)
-
-
-class Guide(User):
-    base_role = User.Role.GUIDE
-
-    objects = GuideManager()
-
-    class Meta:
-        proxy = True
-
-
-class AttendeeManager(BaseUserManager):
-    def get_queryset(self, *args, **kwargs) -> models.QuerySet:
-        qs = super().get_queryset(*args, **kwargs)
-        return qs.filter(role=User.Role.ATTENDEE)
-
-
-class Attendee(User):
-    base_role = User.Role.ATTENDEE
-
-    objects = AttendeeManager()
-
-    class Meta:
-        proxy = True
